6ecb8c643113768b2ae56e5497bbd4404b556014
Droledami <justinmichaux1993@gmail.com>
2023-10-03 14:16:55 +0200

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useState} from "react";
+
+export default function Board(){
+  const [xIsNext,setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    if(squares[i]){
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    if(xIsNext){
+      nextSquares[i] = "X";
+    }else{
+      nextSquares[i] = "0";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  return <>
+    <div className="board-row">
+      <Square value={squares[0]} onSquareClick={()=> handleClick(0)}></Square>
+      <Square value={squares[1]} onSquareClick={()=> handleClick(1)}></Square>
+      <Square value={squares[2]} onSquareClick={()=> handleClick(2)}></Square>
     </div>
-  );
+    <div className="board-row">
+      <Square value={squares[3]} onSquareClick={()=> handleClick(3)}></Square>
+      <Square value={squares[4]} onSquareClick={()=> handleClick(4)}></Square>
+      <Square value={squares[5]} onSquareClick={()=> handleClick(5)}></Square>
+    </div>
+    <div className="board-row">
+      <Square value={squares[6]} onSquareClick={()=> handleClick(6)}></Square>
+      <Square value={squares[7]} onSquareClick={()=> handleClick(7)}></Square>
+      <Square value={squares[8]} onSquareClick={()=> handleClick(8)}></Square>
+    </div>
+    </>
 }
 
-export default App;
+function Square({value, onSquareClick}){
+  return (
+      <button
+          className="square"
+          onClick={onSquareClick}
+      >
+        {value}
+      </button>
+  );
+}
